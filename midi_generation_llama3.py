ac85fc24b87aa7a3c4eaa28998fca81a32a2243b
Nicolas Requena <58339988+NRequena@users.noreply.github.com>
2024-10-29 14:59:04 -0300

    Progress

--- midi_generation_llama3.py
+++ midi_generation_llama3.py
@@ -5,12 +5,12 @@ import mido
 from mido import Message
 import time
 
+####### LLAMA SETUP #######
 USER = 'user'
 ASSISTANT = 'assistant'
 model = 'llama3.2:latest'
 bpm = 60 / 100
 
-# PROMPT
 constant =  "You are a helpful agent that only responds with a single JSON object for MIDI. Do not provide explanations or other text outside of the JSON object. If you need to provide an explanation, generate an appropriate response in JSON format. "
 example = "the exact response of the JSON File shoud be in this format [{'event':'note_on', 'note':'60', 'velocity':64, 'time':0.5}], Velocity should be within 0 and 127 and always be an integer number, not always the same number. "
 end = "Please respond with only a single JSON object and no additional text."
@@ -28,34 +28,8 @@ messages = [
 # API endpoint
 url = "http://localhost:11434/api/chat"
 
-# Request payload
-payload = {
-    "model": model,
-    "messages": messages,
-    "options": {
-        "seed": 4815162342,
-        "temperature": 0.1
-    },
-    "stream": False,
-    
-}
 
-# Send POST request
-response = requests.post(url, json=payload)
-
-# Get response
-result = response.json()
-midi_json = json.loads(result["message"]["content"])
-print(result["message"]["content"])
-#print(type(result["message"]["content"]))
-#print(midi_json)
-#print(midi_json[0]["event"])
-
-
-def add_history(content, role):
-    messages.append({'role': role, 'content': content})
-
-# MIDI PART #
+####### MIDI SETUP #######
 inputs = mido.get_input_names()
 outputs = mido.get_output_names()
 
@@ -65,11 +39,31 @@ print('outputs: ', outputs)
 # create a MIDI out port
 midi_out = mido.open_output("loopMIDI Port 2")
 
-# Play the sequence of notes
-def playMidi(event,note,velocity,time):
-    message = Message(event, note=note, velocity=int(velocity), time=int(time))
-    print(message)
-    #port.send(message)
+#SENDS THE REQUEST AND GETS THE RESPONSE FROM LLAMA3
+def chat(messages):
+    # Request payload
+    payload = {
+        "model": model,
+        "messages": messages,
+        "options": {
+            "seed": 4815162342,
+            "temperature": 0.1
+        },
+        "stream": False,
+    }
+
+    # Send POST request
+    response = requests.post(url, json=payload)
+
+    # Get response
+    result = response.json()
+    midi_json = json.loads(result["message"]["content"])
+    print(result["message"]["content"])
+    return midi_json
+
+
+def add_history(content, role):
+    messages.append({'role': role, 'content': content})
 
 # Generates Midi Notes and Sends it out
 def noteGenerator(velocity, note, tiempo):
@@ -85,17 +79,20 @@ def noteGenerator(velocity, note, tiempo):
     
 
 
-print(type(midi_json[0]["note"]))
-print(type(midi_json[0]["velocity"]))
+#MAKE THIS AND THE CHAT PAYLOAD A FUNCTION SO WE CAN RECURSIVELY CALL IT
 
-#MAKE THIS AND THE CHAT PAYLOAD A FUNCTION SO WE
+# Play the sequence of notes
+def playMidi():
+    midi_json = chat(messages)
+    add_history()
 
-index = 0
-for n in midi_json:
-    llama_event = midi_json[index]["event"]
-    llama_note = int(midi_json[index]["note"])
-    llama_velocity = midi_json[index]["velocity"]
-    llama_time = midi_json[index]["time"]
-    noteGenerator(llama_velocity, llama_note, llama_time)
-    index += 1
-    print(f"Index: {index}, Note: {llama_note}, Velocity: {llama_velocity}, Time: {llama_time}")
+    index = 0
+    for n in midi_json:
+        llama_event = midi_json[index]["event"]
+        llama_note = int(midi_json[index]["note"])
+        llama_velocity = midi_json[index]["velocity"]
+        llama_time = midi_json[index]["time"]
+        noteGenerator(llama_velocity, llama_note, llama_time)
+        index += 1
+        print(f"Index: {index}, Note: {llama_note}, Velocity: {llama_velocity}, Time: {llama_time}")
+    
